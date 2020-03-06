899d0d12bbefe4784c2bdc83fbdf35e5383b3a3c
Emanuel Joívo <emanueljoivo@gmail.com>
2020-03-06 16:37:19 -0300

    Improve stack script and build arrebol image

--- stack/stack.sh
+++ stack/stack.sh
@@ -8,18 +8,42 @@ set -o errexit;
 readonly STACK_NAME=arrebol
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly VERSION=0.0.4
+readonly DOCKERHUB_REPO=emanueljoivo/arrebol
 
 usage () {
     printf "usage: %s deploy | rm | [-h]" "$0"
 }
 
+build_arrebol() {
+  local DB_ADDR=127.0.0.1
+  local DB_USER=arrebol-admin
+  local DB_PASSWD=postgres
+  local DB_NAME=arrebol-db
+  local DB_PORT=5432
+  local WORKER_AMOUNT=5
+
+  local DOCKERFILE_DIR=stack/Dockerfile
+
+  docker build -t "${DOCKERHUB_REPO}":"${VERSION}" \
+            --build-arg DB_ADDR="${DB_ADDR}" \
+            --build-arg DB_USER="${DB_USER}" \
+            --build-arg DB_PASSWD="${DB_PASSWD}" \
+            --build-arg DB_NAME="${DB_NAME}" \
+            --build-arg DB_PORT="${DB_PORT}" \
+            --build-arg WORKER_AMOUNT="${WORKER_AMOUNT}" \
+            --file "${DOCKERFILE_DIR}" .
+}
+
 define_params() {
     case $1 in
         build) shift
-            docker build -t arrebol:"${VERSION}" -f stack/Dockerfile .
+            build_arrebol
             ;;
         deploy) shift
             docker stack deploy -c "${DIR}/"docker-stack.yml "${STACK_NAME}"
+            ;;
+        publish) shift
+            docker push "${DOCKERHUB_REPO}":"${VERSION}"
             ;;
         clean) shift
             docker system prune -f
