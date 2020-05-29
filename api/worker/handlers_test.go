10314988cbd8d2d59ab443d8e101fcacabc1a3ae
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-05-29 11:18:43 -0300

    Updated AddWorker handler test

--- api/worker/handlers_test.go
+++ api/worker/handlers_test.go
@@ -3,6 +3,9 @@ package worker
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker"
+	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker/key"
+	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker/whitelist"
 	"github.com/ufcg-lsd/arrebol-pb/crypto"
 	"log"
 	"net/http"
@@ -13,34 +16,35 @@ import (
 )
 
 func TestWorkerApiAddWorker(t *testing.T) {
-	_ = os.Setenv(crypto.KEYS_PATH, "../../test/keys")
-	var workerSpec = WorkerSpec{
+	_ = os.Setenv(key.KeysPath, "../../test/keys")
+	_ = os.Setenv(whitelist.WhiteListPath, "../../test/whitelist/whitelist")
+	var worker = worker.Worker{
 		ID:      "fake_worker",
 		VCPU:    1.5,
 		RAM:     1024,
 		QueueId: "default",
 	}
 
-	workerSpecBytes, err := json.Marshal(workerSpec)
+	data, err := json.Marshal(worker)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	var mockSignature []uint8
+	var signature []uint8
 	log.Println(os.Getwd())
 	if privKey, err := crypto.GetPrivateKey("../../test/keys/fake_worker"); err != nil {
 		t.Fatal(err)
 	} else {
-		mockSignature, err = crypto.Sign(privKey, workerSpecBytes)
+		signature, err = crypto.Sign(privKey, data)
 	}
 
-	api := New()
-	req, err := http.NewRequest("POST", "/v1/workers", bytes.NewBuffer(workerSpecBytes))
+	api := New(nil)
+	req, err := http.NewRequest("POST", "/v1/workers", bytes.NewBuffer(data))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	req.Header.Set(SIGNATURE_HEADER, string(mockSignature))
+	req.Header.Set(SignatureHeader, string(signature))
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(api.AddWorker)
@@ -53,9 +57,15 @@ func TestWorkerApiAddWorker(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `{"QueueId":"default","Token":"fake_worker#default#1.50#1024"}`
+	expected := `null`
+	expectedToken := "fake_worker#default#1.50#1024"
 	if strings.TrimSpace(rr.Body.String()) != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+	if rr.Header().Get("Token") != expectedToken {
+		t.Errorf("handler returned unexpected token: got %v want %v",
 			rr.Body.String(), expected)
 	}
 }
