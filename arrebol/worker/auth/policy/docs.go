60a22242ed10ca1527e510c825aa86ac6a33ff13
Joívo <emanueljoivo@gmail.com>
2020-07-06 17:55:04 -0300

    Create policy documentation

---
+++ arrebol/worker/auth/policy/docs.go
@@ -0,0 +1,8 @@
+/*
+ *	This package contains authorization policies. Each subpackage is a possible policy.
+ *  Check the Authorizer in auth.go interface.
+ *  Please note that these policies refer specifically to authorization.
+ *  Thus, we have the possibility to extend the code by creating others authentication policies.
+ */
+
+package policy
