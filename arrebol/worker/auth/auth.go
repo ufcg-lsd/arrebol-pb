56fdbaf074db3efc7a2100d6c5f3fc4291bbb2e6
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2020-07-15 17:19:39 -0300

    general cleanup

--- arrebol/worker/auth/auth.go
+++ arrebol/worker/auth/auth.go
@@ -3,12 +3,14 @@ package auth
 import (
 	"encoding/json"
 	"errors"
-	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker"
 	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker/auth/allowlist"
 	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker/auth/token"
-	"github.com/ufcg-lsd/arrebol-pb/arrebol/worker/key"
 	"github.com/ufcg-lsd/arrebol-pb/crypto"
+	"github.com/ufcg-lsd/arrebol-pb/storage"
+	"os"
 )
+
+const KeysPathKey string = "KEYS_PATH"
 
 type Authenticator struct {
 	allowlist allowlist.AllowList
@@ -22,26 +24,48 @@ func NewAuth() *Authenticator {
 	return &auth
 }
 
-func (auth *Authenticator) Authenticate(rawPublicKey string, signature []byte, worker *worker.Worker) (token.Token, error) {
+func (auth *Authenticator) Authenticate(rawPublicKey string, signature []byte, worker *storage.Worker) (token.Token, error) {
 	data, err := json.Marshal(worker)
 	if err != nil {
 		return "", err
 	}
-	publicKey, err := crypto.ParsePublicKeyFromPemStr(rawPublicKey)
-	if err != nil {
-		return "", err
-	}
 
-	err = crypto.Verify(publicKey, data, signature)
-	if err != nil {
+	if err := saveWorkerKey(worker.ID, rawPublicKey); err != nil {return "", err}
+
+	ok, err := CheckSignature(data, signature, worker.ID)
+	if !ok || err != nil {
 		return "", err
 	}
-	if err := key.SavePublicKey(worker.ID, rawPublicKey); err != nil {return "", err}
 
 	return auth.newToken(worker)
 }
 
-func (auth *Authenticator) newToken(worker *worker.Worker) (token.Token, error) {
+func CheckSignature(data []byte, signature []byte, workerId string) (bool, error) {
+	keyName := workerId + ".pub"
+	keyPath := os.Getenv(KeysPathKey) + "/" + keyName
+	publicKey, err := crypto.GetPublicKey(keyPath)
+
+	if err != nil {
+		return false, err
+	}
+
+	err = crypto.Verify(publicKey, data, signature)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func saveWorkerKey(workerId, content string) error {
+	publicKey, err := crypto.ParsePublicKeyFromPemStr(content)
+	if err != nil {return err}
+	path := os.Getenv(KeysPathKey) + "/" + workerId + ".pub"
+	return crypto.SavePublicKey(path, publicKey)
+}
+
+func (auth *Authenticator) newToken(worker *storage.Worker) (token.Token, error) {
 	var t token.Token
 	var err error
 
